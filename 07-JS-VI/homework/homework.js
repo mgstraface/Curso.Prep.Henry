5c794be906f6d6273eabb659035a7149d0302649
mgstraface <matiasgstraface@gmail.com>
2022-03-21 18:36:57 -0300

    Homework resuelto

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -4,7 +4,7 @@ function mayuscula(nombre) {
   //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
   //Tu código:
-  return (nombre.charAt(0).toUpperCase() + nombre.slice(1));
+return nombre[0].toUpperCase() + nombre.slice(1);
   
 }
 
@@ -18,7 +18,7 @@ function operacionMatematica(n1, n2, cb) {
   //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
   //Devolver el callback pasándole como argumentos los números recibidos.
   //Tu código:
-  return cb(n1,n2);
+ return cb(n1,n2);
 }
 
 function sumarArray(numeros, cb) {
